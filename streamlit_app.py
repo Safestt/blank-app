5ca69152d9dbf45e430b73949e5d87ee348cfd65
Safestt <126298996+Safestt@users.noreply.github.com>
2025-02-20 12:38:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,35 @@
 import streamlit as st
 
-def main():
-    st.set_page_config(page_title="Login", page_icon="🔑", layout="centered")
-    
+# Inicializar estado de la página si no existe
+if "page" not in st.session_state:
+    st.session_state.page = "home"
 
-    st.title("Iniciar Sesión")
+st.title("Demo 🚀")
+
+# Página de Inicio
+if st.session_state.page == "home":
+    st.write("Bienvenido, selecciona una opción:")
     
-    username = st.text_input("Usuario", placeholder="Ingrese su usuario")
-    password = st.text_input("Contrassseña", type="password", placeholder="Ingrese su contraseña")
-    
-    if st.button("Ingresar"):
-        st.success("Diseño cargado correctamente")
-    
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button("Iniciar Sesión"):
+            st.session_state.page = "login"
+    with col2:
+        if st.button("Registrarse"):
+            st.session_state.page = "register"
+
+# Página de Registro
+elif st.session_state.page == "register":
+    st.title("Registro")
+    st.write("Aquí iría el formulario de registro")
    
+    username = st.text_input("Usuario", placeholder="Ingrese un usuario para registrarse")
     
-if __name__ == "__main__":
-    main()
+    password = st.text_input("Contraseña", type="password", placeholder="Ingrese su contraseña")
+    password_confirmation = st.text_input("Contraseña confirmacion", type="password", placeholder="Ingrese su contraseña nuevamente")
+    
+    if st.button("Registrarse"):
+        #Logica para autenticacion
+        st.write("Coming soon")
+    if st.button("Volver al inicio"):
+        st.session_state.page = "home"
